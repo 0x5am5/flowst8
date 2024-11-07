182046ca6c9169b939acfef55925553dd085a605
Samuel Gregory <samuelgregory@me.com>
2024-11-07 09:43:37 +0000

    fix gsap and local time

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const navObserver = new IntersectionObserver((entries) => {
 navObserver.observe(watcher)
 
 let options = {
-    timeZone: 'Europe/Rome',
+    timeZone: 'Europe/London',
     hour: 'numeric',
     minute: 'numeric',
     hour12: true
